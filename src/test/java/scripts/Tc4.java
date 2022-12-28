dd06b4e8bb18c724cce9cc6ccc80b83d68467de6
Maheep <Maheep@DESKTOP-QA1AMVG>
2022-12-28 06:14:10 -0800

    project

---
+++ src/test/java/scripts/Tc4.java
@@ -0,0 +1,14 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc4 {
+	
+	@Test
+	public void demo()
+	{
+		Reporter.log("hello java selenium", true);
+	}
+
+}
